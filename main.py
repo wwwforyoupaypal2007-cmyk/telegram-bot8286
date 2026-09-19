f4e93c873516512d13185f93c1da5b683567d1cd
wwwforyoupaypal2007-cmyk <www.foryoupaypal2007@gmail.com>
2026-09-19 22:38:15 +0630

    Update main.py

--- main.py
+++ main.py
@@ -148,9 +148,9 @@ def code_generator(mode):
     elif mode == "7":
         codes = [str(i).zfill(7) for i in range(10000000)]
     elif mode == "8":
-        codes = [str(i).zfill(8) for i in range(100000000)]
+        codes = [str(i).zfill(8) for i in range(10000000)]
     elif mode == "9":
-        codes = [str(i).zfill(9) for i in range(1000000000)]
+        codes = [str(i).zfill(9) for i in range(10000000)]
     elif mode == "alpha6":
         chars = string.ascii_lowercase
         codes = [''.join(random.choices(chars, k=6)) for _ in range(1000000)]
